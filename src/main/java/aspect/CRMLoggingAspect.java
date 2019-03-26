4f4ce35d8ff52da82f192ad54a306788fb03e0ff
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-03-26 09:00:53 +0100

    add aspect to project

---
+++ src/main/java/aspect/CRMLoggingAspect.java
@@ -0,0 +1,38 @@
+package aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+    //setup logger
+        private Logger myLogger = Logger.getLogger(getClass().getName());
+    //setup pointcut declarations
+        @Pointcut("execution(* controller.*.*(..))")
+        private void forControllerPackage(){}
+
+    //do the same for service and dao
+        @Pointcut("execution(* service.*.*(..))")
+        private void forServicePackage(){}
+
+        @Pointcut("execution(* dao.*.*(..))")
+        private void forDaoPackage(){}
+
+        @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+        private void forAppFlow(){}
+
+    //add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+            String theMethod = joinPoint.getSignature().toShortString();
+        System.out.println("====>>> in @Before calling method: "+theMethod);
+    }
+
+    //add @AfterReturning advice
+}
